38901df76aeacf49a1868dccbd51c0a08af159b3
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2024-12-27 17:02:11 +0100

    Updating skills

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -15,23 +15,6 @@ import Segment from '@/components/Segment.vue';
             </ul>
           </Segment>
           <Segment title="LOI | HBO Communication & Multimediadesign" subtitle="2006 – 2012 | Leiderdorp, The Netherlands (Remote) | Did not finish">
-            <ul>
-              <li>Computer skills <strong>(9/10)</strong></li>
-              <li>Communication & social education <strong>(6/10)</strong></li>
-              <li>User interface & web design <strong>(9/10)</strong></li>
-              <li>Writing for the web <strong>(8/10)</strong></li>
-              <li>ICT1 hardware & network components foundation <strong>(7/10)</strong></li>
-              <li>ICT1 practical assignment <strong>(7/10)</strong></li>
-              <li>ICT2 programming introduction <strong>(8/10)</strong></li>
-              <li>ICT2 practical assignment <strong>(9/10)</strong></li>
-              <li>Graphic design <strong>(7/10)</strong></li>
-              <li>Communication theory <strong>(6/10)</strong></li>
-              <li>Marketing <strong>(7/10)</strong></li>
-              <li>Knowledge & information management <strong>(6/10)</strong></li>
-              <li>Professional communication advanced <strong>(7/10)</strong></li>
-              <li>Audio & video <strong>(9/10)</strong></li>
-              <li>Interactive multimedia development <strong>(9/10)</strong></li>
-            </ul>
           </Segment>
           <Segment title="Avans University of Applied Sciences | HBO Informatica" subtitle="2005 – 2006 | ’s-Hertogenbosch, The Netherlands | Did not finish"></Segment>
           <Segment title="Elde College | HAVO Nature & Technic (+ Informatica)" subtitle="2000 – 2005 | Schijndel, The Netherlands"></Segment>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -11,7 +11,7 @@ import Skills from '@/components/Skills.vue';
         <div class="columns__column">
           <Segment title="Hungarian365" href="https://www.hungarian365.com/" target="_blank">
             <p>A Wordpress website focused on teaching the Hungarian language. <Link href="https://www.hungarian365.com/" target="_blank">Hungarian365</Link> uses a theme designed and developed from scratch.</p>
-            <Skills :labels="['PHP','Javascript','HTML','CSS','REST API','Git','JSON','Wordpress']" />
+            <Skills :labels="['PHP','Javascript','HTML','CSS','REST API','Git','Wordpress']" />
           </Segment>
     
           <Segment title="OhMyBrush" href="https://ohmybrush.com/" target="_blank">
@@ -21,12 +21,12 @@ import Skills from '@/components/Skills.vue';
     
           <Segment title="PHP CLI" href="https://github.com/martijnvdb87/php-cli" target="_blank">
             <p>The <Link href="https://github.com/martijnvdb87/php-cli" target="_blank">PHP CLI library</Link> helps you to create CLI applications quicky. The library can also be installed via <Link href="https://packagist.org/packages/martijnvdb/php-cli" target="_blank">Composer</Link>.</p>
-            <Skills :labels="['PHP', 'Git', 'Composer']" />
+            <Skills :labels="['PHP', 'Git']" />
           </Segment>
     
           <Segment title="PHP Image Resize" href="https://github.com/martijnvdb87/php-image-resize" target="_blank">
             <p>This <Link href="https://github.com/martijnvdb87/php-image-resize" target="_blank">PHP Image Resize library</Link> allows you to resize and convert images to a different file format. The supported file types are JPG, PNG, GIF and WebP. The library can also be installed via <Link href="https://packagist.org/packages/martijnvdb/php-image-resize" target="_blank">Composer</Link></p>
-            <Skills :labels="['PHP', 'Git', 'Composer']" />
+            <Skills :labels="['PHP', 'Git']" />
           </Segment>
           
           <Segment title="Wordpress Plugin Payhip Products" href="https://github.com/martijnvdb87/wordpress-plugin-payhip-products" target="_blank">
@@ -36,7 +36,7 @@ import Skills from '@/components/Skills.vue';
     
           <Segment title="Wordpress Plugin Tools" href="https://github.com/martijnvdb87/wordpress-plugin-tools" target="_blank">
             <p>The <Link href="https://github.com/martijnvdb87/wordpress-plugin-tools" target="_blank">Wordpress Plugin Tools library</Link> was made to simplify the proces of creating custom post types and settings pages in Wordpress. The library can also be installed via <Link href="https://packagist.org/packages/martijnvdb/wordpress-plugin-tools" target="_blank">Composer</Link></p>
-            <Skills :labels="['PHP', 'HTML', 'Javascript', 'CSS', 'Git', 'Wordpress', 'Composer']" />
+            <Skills :labels="['PHP', 'HTML', 'Javascript', 'CSS', 'Git', 'Wordpress']" />
           </Segment>
     
           <Segment title="mRouter" href="https://github.com/martijnvdb87/mrouter" target="_blank">
@@ -51,7 +51,7 @@ import Skills from '@/components/Skills.vue';
     
           <Segment title="MagyarBlogok" href="https://web.archive.org/web/20181129093940/http://www.magyarblogok.hu/" target="_blank">
             <p>One of the biggest curated Hungarian blogging feed. <Link href="https://web.archive.org/web/20181129093940/http://www.magyarblogok.hu/" target="_blank">MagyarBlogok</Link> scapped over a hundred blogs and YouTube channels multiple times a day to get titles, URL's, thumbnails and descriptions of new posts and videos. The blogs were scrapped based on historical post activity frequency, so more active blogs would be scrapped more frequently.</p>
-            <Skills :labels="['PHP', 'Javascript', 'SQL', 'HTML', 'CSS', 'REST API', 'JSON']" />
+            <Skills :labels="['PHP', 'Javascript', 'SQL', 'HTML', 'CSS', 'REST API']" />
           </Segment>
         </div>
       </section>
